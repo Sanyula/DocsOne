df21eeccd93fb6d53dac07effed875f13bcdfd78
Sanyula <henricsanyu@gmail.com>
2021-06-27 02:10:14 +0300

    Add project files.

---
+++ DocsOne/Controllers/DocsController.cs
@@ -0,0 +1,199 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DocsOne.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class DocsController : ControllerBase
+    {
+     
+        private readonly ILogger<DocsController> _logger;
+        public DocsController(ILogger<DocsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Welcome()
+        {
+            return Ok("Welcome to the docs api");
+
+        }
+            [HttpGet]
+        [Route("getdoc")]
+        public HttpResponseMessage Get()
+        {
+            HttpRequestMessage httpRequestMessage;
+
+            string fullPath = @"D:\VS\sample.docx";
+            HttpResponseMessage responseMsg;
+            byte[] docBytes = System.IO.File.ReadAllBytes(fullPath);
+            var dataStream = new MemoryStream(docBytes);
+            responseMsg = new HttpResponseMessage(HttpStatusCode.OK);
+            responseMsg.Content = new StreamContent(dataStream);
+            responseMsg.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            responseMsg.Content.Headers.ContentDisposition.FileName = "sample.docx";
+            responseMsg.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            return responseMsg;
+
+        }
+
+        [HttpPost]
+        [Route("toHtml")]
+        public IActionResult docToHtml()
+        {
+                string htmlText = string.Empty;
+                DocProcessor processor = new DocProcessor();
+                try
+                {
+                    htmlText = processor.ParseDOCX();
+                }
+                catch (OpenXmlPackageException e)
+                {
+                    if (e.ToString().Contains("Invalid Hyperlink"))
+                    {
+                        htmlText = processor.ParseDOCX();
+                    }
+                }
+                return Ok(htmlText);
+        }
+
+        [HttpPost]
+        [Route("toDoc")]
+        public IActionResult htmlToDoc()
+        {
+            string htmlText = string.Empty;
+            DocProcessor processor = new DocProcessor();
+            try
+            {
+                 processor.ConvertToDocx();
+            }
+            catch (OpenXmlPackageException e)
+            {
+                if (e.ToString().Contains("Invalid Hyperlink"))
+                {
+                    processor.ConvertToDocx();
+                }
+            }
+            return Ok("Successful");
+        }
+
+        [HttpPost, DisableRequestSizeLimit]
+        [Route("makehtml")]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = "uploads"; // Path.Combine("Resources", "Docs");
+                var tempDi = @"D:\VS\";
+                var pathToSave = Path.Combine(tempDi, folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    string localFilePath = fullPath;
+
+                    string htmlText = string.Empty;
+                    DocProcessor processor = new DocProcessor();
+                    try
+                    {
+                        htmlText = processor.ParseDOCX();
+                    }
+                    catch (OpenXmlPackageException e)
+                    {
+                        if (e.ToString().Contains("Invalid Hyperlink"))
+                        {
+                            htmlText = processor.ParseDOCX();
+                        }
+                    }
+
+                   return Ok(new { htmlText });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+        
+        [HttpPost]
+        [Route("makedoc")]
+        public HttpResponseMessage makeDoc()
+        {
+            HttpResponseMessage responseMsg;
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = "uploads"; // Path.Combine("Resources", "Docs");
+                var tempDi = @"D:\VS\";
+                var pathToSave = Path.Combine(tempDi, folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    string localFilePath = fullPath;
+
+                    string htmlText = string.Empty;
+                    DocProcessor processor = new DocProcessor();
+                    try
+                    {
+                        htmlText = processor.ParseDOCX();
+                    }
+                    catch (OpenXmlPackageException e)
+                    {
+                        if (e.ToString().Contains("Invalid Hyperlink"))
+                        {
+                            htmlText = processor.ParseDOCX();
+                        }
+                    }
+
+                    byte[] docBytes = System.IO.File.ReadAllBytes(fullPath);
+                    var dataStream = new MemoryStream(docBytes);
+                    responseMsg = new HttpResponseMessage(HttpStatusCode.OK);
+                    responseMsg.Content = new StreamContent(dataStream);
+                    responseMsg.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+                    responseMsg.Content.Headers.ContentDisposition.FileName = file.FileName;
+                    responseMsg.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    //
+                }
+                else
+                {
+                     responseMsg =  new HttpResponseMessage(HttpStatusCode.BadRequest);
+                }
+            }
+            catch (Exception ex)
+            {
+                responseMsg = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+
+            return responseMsg;
+        }
+
+    }
+}
---
+++ DocsOne/DocProcessor.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using OpenXmlPowerTools.HtmlToWml;
+
+namespace DocsOne
+{
+    class DocProcessor
+    {
+        public string ParseDOCX()
+        {
+            FileInfo fileInfo = new FileInfo(@"D:\VS\sample.docx");
+            try
+            {
+                    
+                byte[] byteArray = File.ReadAllBytes(fileInfo.FullName);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(byteArray, 0, byteArray.Length);
+                    using (WordprocessingDocument wDoc =
+                                              WordprocessingDocument.Open(memoryStream, true))
+                    {
+                        int imageCounter = 0;
+                        var pageTitle = fileInfo.FullName;
+                        var part = wDoc.CoreFilePropertiesPart;
+                        if (part != null)
+                            pageTitle = (string)part.GetXDocument()
+                                                    .Descendants(DC.title)
+                                                    .FirstOrDefault() ?? fileInfo.FullName;
+
+                        WmlToHtmlConverterSettings settings = new WmlToHtmlConverterSettings()
+                        {
+                            AdditionalCss = "body { margin: 1cm auto; max-width: 20cm; padding: 0; }",
+                            PageTitle = pageTitle,
+                            FabricateCssClasses = true,
+                            CssClassPrefix = "pt-",
+                            RestrictToSupportedLanguages = false,
+                            RestrictToSupportedNumberingFormats = false,
+                            ImageHandler = imageInfo =>
+                            {
+                                ++imageCounter;
+                                string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+                                ImageFormat imageFormat = null;
+                                if (extension == "png") imageFormat = ImageFormat.Png;
+                                else if (extension == "gif") imageFormat = ImageFormat.Gif;
+                                else if (extension == "bmp") imageFormat = ImageFormat.Bmp;
+                                else if (extension == "jpeg") imageFormat = ImageFormat.Jpeg;
+                                else if (extension == "tiff")
+                                {
+                                    extension = "gif";
+                                    imageFormat = ImageFormat.Gif;
+                                }
+                                else if (extension == "x-wmf")
+                                {
+                                    extension = "wmf";
+                                    imageFormat = ImageFormat.Wmf;
+                                }
+
+                                if (imageFormat == null) return null;
+
+                                string base64 = null;
+                                try
+                                {
+                                    using (MemoryStream ms = new MemoryStream())
+                                    {
+                                        imageInfo.Bitmap.Save(ms, imageFormat);
+                                        var ba = ms.ToArray();
+                                        base64 = System.Convert.ToBase64String(ba);
+                                    }
+                                }
+                                catch (System.Runtime.InteropServices.ExternalException)
+                                { return null; }
+
+                                ImageFormat format = imageInfo.Bitmap.RawFormat;
+                                ImageCodecInfo codec = ImageCodecInfo.GetImageDecoders()
+                                                          .First(c => c.FormatID == format.Guid);
+                                string mimeType = codec.MimeType;
+
+                                string imageSource =
+                                       string.Format("data:{0};base64,{1}", mimeType, base64);
+
+                                XElement img = new XElement(Xhtml.img,
+                                      new XAttribute(NoNamespace.src, imageSource),
+                                      imageInfo.ImgStyleAttribute,
+                                      imageInfo.AltText != null ?
+                                           new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
+                                return img;
+                            }
+                        };
+
+                        XElement htmlElement = WmlToHtmlConverter.ConvertToHtml(wDoc, settings);
+                        var html = new XDocument(new XDocumentType("html", null, null, null),
+                                                                                    htmlElement);
+                        var htmlString = html.ToString(SaveOptions.DisableFormatting);
+                        return htmlString;
+                    }
+                }
+            }
+            catch
+            {
+                return "File contains corrupt data";
+            }
+        }
+
+        public string FixUri(string brokenUri)
+        {
+            string newURI = string.Empty;
+            if (brokenUri.Contains("mailto:"))
+            {
+                int mailToCount = "mailto:".Length;
+                brokenUri = brokenUri.Remove(0, mailToCount);
+                newURI = brokenUri;
+            }
+            else
+            {
+                newURI = " ";
+            }
+            return newURI;
+        }
+
+        public  void ConvertToDocx()
+        {
+            var n = DateTime.Now;
+            var tempDi = new DirectoryInfo(string.Format(@"D:\VS\DocOutput-{0:00}-{1:00}-{2:00}-{3:00}{4:00}{5:00}", n.Year - 2000, n.Month, n.Day, n.Hour, n.Minute, n.Second));
+            tempDi.Create();
+            string destinationDir = tempDi.FullName;
+            //string destinationDir = @"D:\VS\";
+            var sourceHtmlFi = new FileInfo(@"D:\VS\sample.html");
+            var sourceImageDi = new DirectoryInfo(destinationDir);
+
+            var destDocxFi = new FileInfo(Path.Combine(destinationDir, sourceHtmlFi.Name.Replace(".html", ".docx")));
+
+            XElement html = HtmlToWmlReadAsXElement.ReadAsXElement(sourceHtmlFi);
+
+            string usedAuthorCss = HtmlToWmlConverter.CleanUpCss((string)html.Descendants().FirstOrDefault(d => d.Name.LocalName.ToLower() == "style"));
+
+            HtmlToWmlConverterSettings settings = HtmlToWmlConverter.GetDefaultSettings();
+            // image references in HTML files contain the path to the subdir that contains the images, so base URI is the name of the directory
+            // that contains the HTML files
+            settings.BaseUriForImages = sourceHtmlFi.DirectoryName;
+
+            WmlDocument doc = HtmlToWmlConverter.ConvertHtmlToWml(defaultCss, usedAuthorCss, userCss, html, settings);
+            doc.SaveAs(destDocxFi.FullName);
+        }
+
+        public class HtmlToWmlReadAsXElement
+        {
+            public static XElement ReadAsXElement(FileInfo sourceHtmlFi)
+            {
+                string htmlString = File.ReadAllText(sourceHtmlFi.FullName);
+                XElement html = null;
+                try
+                {
+                    html = XElement.Parse(htmlString);
+                }
+#if USE_HTMLAGILITYPACK
+            catch (XmlException)
+            {
+                HtmlDocument hdoc = new HtmlDocument();
+                hdoc.Load(sourceHtmlFi.FullName, Encoding.Default);
+                hdoc.OptionOutputAsXml = true;
+                hdoc.Save(sourceHtmlFi.FullName, Encoding.Default);
+                StringBuilder sb = new StringBuilder(File.ReadAllText(sourceHtmlFi.FullName, Encoding.Default));
+                sb.Replace("&amp;", "&");
+                sb.Replace("&nbsp;", "\xA0");
+                sb.Replace("&quot;", "\"");
+                sb.Replace("&lt;", "~lt;");
+                sb.Replace("&gt;", "~gt;");
+                sb.Replace("&#", "~#");
+                sb.Replace("&", "&amp;");
+                sb.Replace("~lt;", "&lt;");
+                sb.Replace("~gt;", "&gt;");
+                sb.Replace("~#", "&#");
+                File.WriteAllText(sourceHtmlFi.FullName, sb.ToString(), Encoding.Default);
+                html = XElement.Parse(sb.ToString());
+            }
+#else
+                catch (XmlException e)
+                {
+                    throw e;
+                }
+#endif
+                // HtmlToWmlConverter expects the HTML elements to be in no namespace, so convert all elements to no namespace.
+                html = (XElement)ConvertToNoNamespace(html);
+                return html;
+            }
+
+            private static object ConvertToNoNamespace(XNode node)
+            {
+                XElement element = node as XElement;
+                if (element != null)
+                {
+                    return new XElement(element.Name.LocalName,
+                        element.Attributes().Where(a => !a.IsNamespaceDeclaration),
+                        element.Nodes().Select(n => ConvertToNoNamespace(n)));
+                }
+                return node;
+            }
+        }
+
+static string defaultCss =
+            @"html, address,
+blockquote,
+body, dd, div,
+dl, dt, fieldset, form,
+frame, frameset,
+h1, h2, h3, h4,
+h5, h6, noframes,
+ol, p, ul, center,
+dir, hr, menu, pre { display: block; unicode-bidi: embed }
+li { display: list-item }
+head { display: none }
+table { display: table }
+tr { display: table-row }
+thead { display: table-header-group }
+tbody { display: table-row-group }
+tfoot { display: table-footer-group }
+col { display: table-column }
+colgroup { display: table-column-group }
+td, th { display: table-cell }
+caption { display: table-caption }
+th { font-weight: bolder; text-align: center }
+caption { text-align: center }
+body { margin: auto; }
+h1 { font-size: 2em; margin: auto; }
+h2 { font-size: 1.5em; margin: auto; }
+h3 { font-size: 1.17em; margin: auto; }
+h4, p,
+blockquote, ul,
+fieldset, form,
+ol, dl, dir,
+menu { margin: auto }
+a { color: blue; }
+h5 { font-size: .83em; margin: auto }
+h6 { font-size: .75em; margin: auto }
+h1, h2, h3, h4,
+h5, h6, b,
+strong { font-weight: bolder }
+blockquote { margin-left: 40px; margin-right: 40px }
+i, cite, em,
+var, address { font-style: italic }
+pre, tt, code,
+kbd, samp { font-family: monospace }
+pre { white-space: pre }
+button, textarea,
+input, select { display: inline-block }
+big { font-size: 1.17em }
+small, sub, sup { font-size: .83em }
+sub { vertical-align: sub }
+sup { vertical-align: super }
+table { border-spacing: 2px; }
+thead, tbody,
+tfoot { vertical-align: middle }
+td, th, tr { vertical-align: inherit }
+s, strike, del { text-decoration: line-through }
+hr { border: 1px inset }
+ol, ul, dir,
+menu, dd { margin-left: 40px }
+ol { list-style-type: decimal }
+ol ul, ul ol,
+ul ul, ol ol { margin-top: 0; margin-bottom: 0 }
+u, ins { text-decoration: underline }
+br:before { content: ""\A""; white-space: pre-line }
+center { text-align: center }
+:link, :visited { text-decoration: underline }
+:focus { outline: thin dotted invert }
+/* Begin bidirectionality settings (do not change) */
+BDO[DIR=""ltr""] { direction: ltr; unicode-bidi: bidi-override }
+BDO[DIR=""rtl""] { direction: rtl; unicode-bidi: bidi-override }
+*[DIR=""ltr""] { direction: ltr; unicode-bidi: embed }
+*[DIR=""rtl""] { direction: rtl; unicode-bidi: embed }
+
+";
+
+        static string userCss = @"";
+    }
+}
